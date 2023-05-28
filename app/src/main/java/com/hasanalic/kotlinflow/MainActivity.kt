a389198e27df4d15857d9752e56bf14af8ce57de
Hasan Ali Caliskan <hasanali7295@gmail.com>
2023-05-28 14:02:19 +0300

    added collectAsState

--- app/src/main/java/com/hasanalic/kotlinflow/MainActivity.kt
+++ app/src/main/java/com/hasanalic/kotlinflow/MainActivity.kt
@@ -3,8 +3,17 @@ package com.hasanalic.kotlinflow
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.sp
 import com.hasanalic.kotlinflow.ui.theme.KotlinFlowTheme
 
 class MainActivity : ComponentActivity() {
@@ -12,8 +21,21 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             KotlinFlowTheme {
-                Surface(color = MaterialTheme.colorScheme.background) {
+                val viewModel: MyViewModel by viewModels()
+                // xml'de -> val counter = viewModel.countDownTimer.collect(initial = 10)
+                // collectAsState -> değeri alıyor ve state'e çeviriyor.
+                val counter = viewModel.countDownTimer.collectAsState(initial = 10)
 
+                Surface(color = MaterialTheme.colorScheme.background) {
+                    Box(modifier = Modifier.fillMaxSize()) {
+                        Text(
+                            text = counter.value.toString(),
+                            fontSize = 26.sp,
+                            textAlign = TextAlign.Center,
+                            modifier = Modifier.align(Alignment.Center)
+                        )
+                    }
+                    
                 }
             }
         }
