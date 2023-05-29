4dd4a1574e504b34ed2983df545b8ceece03d33c
Hasan Ali Caliskan <hasanali7295@gmail.com>
2023-05-29 13:40:51 +0300

    updated CounterActivity

--- app/src/main/java/com/hasanalic/kotlinflow/CounterActivity.kt
+++ app/src/main/java/com/hasanalic/kotlinflow/CounterActivity.kt
@@ -3,10 +3,13 @@ package com.hasanalic.kotlinflow
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.runtime.getValue
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
@@ -25,7 +28,9 @@ class CounterActivity: ComponentActivity() {
                     val navController = rememberNavController()
                     NavHost(navController = navController, startDestination = "firstScreen") {
                         composable(route = "firstScreen") {
-                            FirstScreen(time = 10) {
+                            val viewmodel = viewModel<FSViewModel>()
+                            val time by viewmodel.counter.collectAsState()
+                            FirstScreen(time = time) {
                                 navController.navigate("secondScreen")
                             }
                         }
